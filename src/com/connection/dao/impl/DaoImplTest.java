54fc1db8866d2fdbd734ebc1f46e6a8b849ce9ea
ChenWorkSpace <chen1494518303@outlook.com>
2017-01-01 13:32:56 +0800

    一次完整的提交

--- src/com/connection/dao/impl/DaoImplTest.java
+++ src/com/connection/dao/impl/DaoImplTest.java
@@ -49,9 +49,7 @@ public class DaoImplTest {
 			e1.printStackTrace();
 		}
 	}
-	 System.out.println("Hello World");
-	 System.out.println("你好 世界！  ");
-	 System.out.println("你好 世界！  ");
+	 
 	 	}
 	
 
